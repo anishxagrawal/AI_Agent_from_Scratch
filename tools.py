2cd0d0e10982d4cadb4abfb24e60046b2ddf53aa
anishxagrawal <agrawalanish052@gmail.com>
2025-12-11 21:38:12 +0530

    added search_func

--- tools.py
+++ tools.py
@@ -1,11 +1,15 @@
 from langchain_community.tools import DuckDuckGoSearchRun
 from langchain_community.tools.wikipedia.tool import WikipediaQueryRun
 from langchain_community.utilities.wikipedia import WikipediaAPIWrapper
-
 from langchain.tools import Tool
 from datetime import datetime
 
 search = DuckDuckGoSearchRun()
+
+def search_func(query: str):
+    """Search the web using DuckDuckGo."""
+    return search.run(query)
+
 search_tool = Tool(
     name="search",
     func=search.run,
